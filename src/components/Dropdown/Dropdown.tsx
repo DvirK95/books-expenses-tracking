cfc5cbc0d5da358d5a1e368b76b0f586727ffa56
DvirK95 <dvirkarl@gmail.com>
2023-07-16 19:39:35 +0300

    fix

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,73 +1,43 @@
-import React, { useState } from 'react';
+import React, { ReactElement, useState } from 'react';
+import './Dropdown.css';
 
-type DropdownProps = {
-  children: React.ReactNode;
-};
-
-interface DropdownComponent extends React.FC<DropdownProps> {
-  Toggle: React.FC<DropdownToggleProps>;
-  Menu: React.FC<DropdownMenuProps>;
+interface DropdownProps {
+  trigger: ReactElement;
+  menu: ReactElement[];
 }
 
-const Dropdown: DropdownComponent = ({ children }) => {
-  const [isOpen, setIsOpen] = useState(false);
+const Dropdown: React.FC<DropdownProps> = ({ trigger, menu }) => {
+  const [open, setOpen] = useState<boolean>(false);
 
-  const handleToggle = () => {
-    setIsOpen(!isOpen);
+  const handleOpen = () => {
+    setOpen(!open);
   };
 
   return (
     <div className="dropdown">
-      <div className="dropdown-toggle" onClick={handleToggle}>
-        {React.Children.map(children, (child) => {
-          if (React.isValidElement(child)) {
-            if (child.type === DropdownToggle) {
-              return React.cloneElement(child, { onClick: handleToggle } as DropdownToggleProps); // Cast to DropdownToggleProps
-            }
-          }
-          return null;
-        })}
-      </div>
-      {isOpen && (
+      {React.cloneElement(trigger, {
+        onClick: handleOpen,
+      })}
+      {open ? (
         <div className="dropdown-menu">
-          {React.Children.map(children, (child) => {
-            if (React.isValidElement(child)) {
-              if (child.type === DropdownMenu) {
-                return React.cloneElement(child, { isOpen } as DropdownMenuProps); // Cast to DropdownMenuProps
-              }
-            }
-            return null;
-          })}
+          <ul className="menu">
+            {menu.map((menuItem, index) => (
+              <li key={index} className="menu-item">
+                {React.cloneElement(menuItem, {
+                  onClick: () => {
+                    if (menuItem.props.onClick) {
+                      menuItem.props.onClick();
+                    }
+                    setOpen(false);
+                  },
+                })}
+              </li>
+            ))}
+          </ul>
         </div>
-      )}
+      ) : null}
     </div>
   );
 };
-
-type DropdownToggleProps = {
-  id: string;
-  onClick: () => void;
-  children: React.ReactNode;
-};
-
-const DropdownToggle: React.FC<DropdownToggleProps> = ({ id, onClick, children }) => {
-  return (
-    <button type="button" id={id} onClick={onClick}>
-      {children}
-    </button>
-  );
-};
-
-type DropdownMenuProps = {
-  isOpen: boolean;
-  children: React.ReactNode;
-};
-
-const DropdownMenu: React.FC<DropdownMenuProps> = ({ isOpen, children }) => {
-  return isOpen ? <div className="dropdown-menu">{children}</div> : null;
-};
-
-Dropdown.Toggle = DropdownToggle;
-Dropdown.Menu = DropdownMenu;
 
 export default Dropdown;
--- src/components/My-Books/BookExpenses/BookExpenses.tsx
+++ src/components/My-Books/BookExpenses/BookExpenses.tsx
@@ -1,8 +1,9 @@
-import {  useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Card from '../../Cards/Card';
 import { Col, Row } from '../../Grid/Grid';
 import { RootState } from '../../../store/store';
-import './BookExpenses.css'
+import Dropdown from '../../Dropdown/Dropdown';
+import './BookExpenses.css';
 
 function BookExpenses() {
   const BooksArr = useSelector((state: RootState) => state.book.books);
@@ -17,17 +18,30 @@ function BookExpenses() {
               <Row>
                 <Col size={3}>{book.name}</Col>
                 <Col size={3}>{book.author}</Col>
-                <Col size={3}>{book.purchaseDate.toISOString().split('T')[0] || ''}</Col>
+                <Col size={3}>
+                  {book.purchaseDate.toISOString().split('T')[0] || ''}
+                </Col>
                 <Col size={3}>${book.price.toFixed(2)}</Col>
               </Row>
             </Col>
             <Col size={1}>
               <Row>
-              <Col size={1}><button>+</button></Col>
+                <Col size={1}>
+                  <Dropdown
+                    trigger={<button>Dropdown</button>}
+                    menu={[
+                      <Card>
+                        <button className="card-btn">edit</button>
+                        <button className="card-btn">remove</button>
+                      </Card>,
+                    ]}
+                  />
+                </Col>
               </Row>
             </Col>
           </Row>
-        </Card.Body>))}
+        </Card.Body>
+      ))}
     </Card>
   );
 }
