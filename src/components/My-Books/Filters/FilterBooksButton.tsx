e706a93616e3b78c0de492bfb4a8418216316d6c
DvirK95 <dvirkarl@gmail.com>
2023-07-19 07:40:59 +0300

    move to other computer

--- src/components/My-Books/Filters/FilterBooksButton.tsx
+++ src/components/My-Books/Filters/FilterBooksButton.tsx
@@ -2,7 +2,8 @@ import { FilterBooksButtonProps } from './hooks/interfaces';
 function FilterBooksButton({ filterButton }: FilterBooksButtonProps) {
   return (
     <div className="btn-wrapper-center">
-      <button className="btn btn-filter">{filterButton.name}</button>
+      {/*<button className="btn btn-filter">{filterButton.name}</button>*/}
+      <input type="text" placeholder={filterButton.name} />
     </div>
   );
 }
